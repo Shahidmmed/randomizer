06c91da7ca801093966f51b449e40c81814c4c3c
Abdul-Shahid Mohammed <shahidmmed@gmail.com>
2024-01-24 15:13:07 +0000

    n

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
-import { defineConfig } from "vite";
+// vite.config.js
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: "/vals-v4/",
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["three"],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 600, // set your desired size limit
+  },
 });
